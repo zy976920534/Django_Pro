d11b126d074d25cf3f1a34783e8048db3e92bb93
zy <zy@163.com>
2018-09-10 17:43:28 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
-def django.http import HttpResponse
+from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse('index')
+
+def login(request):
+    return redirect('/index')
